51c3f4f95763cf38c030338bdf0228355f63185f
berkbaysal <berkbaysal@gmail.com>
2022-10-24 20:01:35 +0300

    fixed a bug with ssl

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import ServerListener from './components/ServerListener';
 import { server } from './static/gameValues';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-const socket = io(server);
+const socket = io(server, { secure: true });
 
 root.render(
   <GameContextProvider socket={socket}>
